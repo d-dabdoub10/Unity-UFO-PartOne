e513611f7597ffe8fca9c720642f0dca65e60c59
d-dabdoub10 <42654129+d-dabdoub10@users.noreply.github.com>
2019-02-17 15:49:20 -0500

    Add files via upload

---
+++ EscapeKey.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscapeKey : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
